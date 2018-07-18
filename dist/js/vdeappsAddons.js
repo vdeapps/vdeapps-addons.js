f6c03cdf370650bea52740a1e39fda5beed742d9
vdeapps <vdeapps@riseup.net>
2018-07-18 11:17:26 +0200

    first release

---
+++ dist/js/vdeappsAddons.js
@@ -0,0 +1,184 @@
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var vdeappsAddons = function () {
+    function vdeappsAddons() {
+        _classCallCheck(this, vdeappsAddons);
+
+        this.vendor = 'vdeapps';
+        this.addons = {};
+        this.addonsIndex = {};
+    }
+
+    /**
+     * Init all Addons
+     */
+
+
+    _createClass(vdeappsAddons, [{
+        key: "init",
+        value: function init() {
+            for (var name in this.addonsIndex) {
+
+                console.info("Run init: " + name);
+
+                try {
+                    this[name].init();
+                } catch (error) {}
+            }
+        }
+
+        /**
+         * Run all ready when document is ready
+         */
+
+    }, {
+        key: "onReady",
+        value: function onReady() {
+
+            for (var name in this.addonsIndex) {
+
+                console.info("Run init: " + name);
+
+                try {
+                    this[name].onReady();
+                } catch (error) {}
+            }
+        }
+    }, {
+        key: "onUnload",
+        value: function onUnload() {
+            for (var name in this.addonsIndex) {
+
+                console.info("Run init: " + name);
+
+                try {
+                    this[name].onUnload();
+                } catch (error) {}
+            }
+        }
+
+        /**
+         * Append new vdeappsAddon
+         * @param vdeappsAddonClass
+         * @param position order of object
+         */
+
+    }, {
+        key: "addAddon",
+        value: function addAddon(vdeappsAddonObject) {
+            var position = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+
+            console.info(vdeappsAddonObject);
+
+            try {
+                if (vdeappsAddonObject.getParentName() === 'vdeappsAddonAbstract') {
+
+                    console.info("Addon ok");
+                    var name = vdeappsAddonObject.getName();
+
+                    // Test if already loaded
+                    // if (typeof this[name] != undefined) {
+                    //     return this;
+                    // }
+
+                    // Assign addon
+                    this[name] = vdeappsAddonObject;
+
+                    // Ajout de l'addon
+                    this.addons[name] = true;
+
+                    // Order of object
+                    this.addonsIndex[helper.str_pad(position, 3, '0', 'STR_PAD_LEFT') + '-' + name] = name;
+                }
+            } catch (error) {
+                throw new Error("vdeappsAddonObject parameter is not from vdeappsAddonAbstract !");
+            }
+
+            return this;
+        }
+
+        /**
+         * Return an addon
+         * @param addonName
+         * @returns {*}
+         */
+
+    }, {
+        key: "get",
+        value: function get(addonName) {
+            if (_typeof(this.addons[addonName]) != undefined) {
+                return this.addons[addonName];
+            } else {
+                throw new Error(addonName + " object not found !");
+            }
+        }
+    }]);
+
+    return vdeappsAddons;
+}();
+
+/**
+ * Abstract class for Addon vdeapps
+ */
+
+
+var vdeappsAddonAbstract = function () {
+    function vdeappsAddonAbstract() {
+        _classCallCheck(this, vdeappsAddonAbstract);
+
+        this.name = this.constructor;
+
+        if (this.constructor === vdeappsAddonAbstract) {
+            throw new Error("Can't instantiate abstract class " + this.constructor + "!");
+        }
+    }
+
+    _createClass(vdeappsAddonAbstract, [{
+        key: "getParentName",
+        value: function getParentName() {
+            return 'vdeappsAddonAbstract';
+        }
+    }, {
+        key: "setName",
+        value: function setName(addonName) {
+            this.name = addonName;
+        }
+    }, {
+        key: "getName",
+        value: function getName() {
+            return this.name;
+        }
+
+        /**
+         * Init vars
+         */
+
+    }, {
+        key: "init",
+        value: function init() {}
+
+        /**
+         * load onready
+         */
+
+    }, {
+        key: "onReady",
+        value: function onReady() {}
+
+        /**
+         * Run when unload page
+         */
+
+    }, {
+        key: "onUnload",
+        value: function onUnload() {}
+    }]);
+
+    return vdeappsAddonAbstract;
+}();
---
+++ src/vdeappsAddons.js
@@ -0,0 +1,153 @@
+class vdeappsAddons {
+    
+    constructor() {
+        this.vendor = 'vdeapps';
+        this.addons = {};
+        this.addonsIndex = {};
+    }
+    
+    /**
+     * Init all Addons
+     */
+    init() {
+        for (let name in this.addonsIndex) {
+            
+            console.info("Run init: " + name)
+            
+            try {
+                this[name].init()
+            }
+            catch (error) {
+            
+            }
+        }
+    }
+    
+    /**
+     * Run all ready when document is ready
+     */
+    onReady() {
+        
+        for (let name in this.addonsIndex) {
+            
+            console.info("Run init: " + name)
+            
+            try {
+                this[name].onReady()
+            }
+            catch (error) {
+            
+            }
+        }
+        
+    }
+    
+    onUnload() {
+        for (let name in this.addonsIndex) {
+            
+            console.info("Run init: " + name)
+            
+            try {
+                this[name].onUnload()
+            }
+            catch (error) {
+            
+            }
+        }
+    }
+    
+    /**
+     * Append new vdeappsAddon
+     * @param vdeappsAddonClass
+     * @param position order of object
+     */
+    addAddon(vdeappsAddonObject, position = 10) {
+        console.info(vdeappsAddonObject);
+        
+        try {
+            if (vdeappsAddonObject.getParentName() === 'vdeappsAddonAbstract') {
+                
+                console.info("Addon ok")
+                let name = vdeappsAddonObject.getName();
+                
+                // Test if already loaded
+                // if (typeof this[name] != undefined) {
+                //     return this;
+                // }
+                
+                // Assign addon
+                this[name] = vdeappsAddonObject;
+                
+                // Ajout de l'addon
+                this.addons[name] = true;
+                
+                // Order of object
+                this.addonsIndex[helper.str_pad(position, 3, '0', 'STR_PAD_LEFT') + '-' + name] = name;
+            }
+        }
+        catch (error) {
+            throw new Error("vdeappsAddonObject parameter is not from vdeappsAddonAbstract !");
+        }
+        
+        return this;
+    }
+    
+    /**
+     * Return an addon
+     * @param addonName
+     * @returns {*}
+     */
+    get(addonName) {
+        if (typeof this.addons[addonName] != undefined) {
+            return this.addons[addonName];
+        }
+        else {
+            throw new Error(addonName + " object not found !");
+        }
+    }
+}
+
+/**
+ * Abstract class for Addon vdeapps
+ */
+class vdeappsAddonAbstract {
+    
+    constructor() {
+        this.name = this.constructor;
+        
+        if (this.constructor === vdeappsAddonAbstract) {
+            throw new Error("Can't instantiate abstract class " + this.constructor + "!");
+        }
+    }
+    
+    getParentName() {
+        return 'vdeappsAddonAbstract';
+    }
+    
+    setName(addonName) {
+        this.name = addonName;
+    }
+    
+    getName() {
+        return (this.name)
+    }
+    
+    /**
+     * Init vars
+     */
+    init() {
+    }
+    
+    /**
+     * load onready
+     */
+    onReady() {
+    }
+    
+    /**
+     * Run when unload page
+     */
+    onUnload() {
+    }
+}
+
